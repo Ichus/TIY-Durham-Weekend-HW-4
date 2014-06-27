614322c596ec291968dbf910548a281144fa0657
Ichus <Ichus.local>
2014-06-27 19:30:32 -0400

    Markdown works now.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
   def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]
-    Redcarpet.new(text, *options).to_html.html_safe
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    extensions = { autolink: true, no_intra_emphasis: true }
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
   end
+
+  # def markdown(text)
+  #   options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]
+  #   Redcarpet::Markdown.new(text, *options).to_html.html_safe
+  # end
 end
