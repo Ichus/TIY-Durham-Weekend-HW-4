05b27fafdad78c2299b4e538c7a8a2db65c73901
Ichus <Ichus.local>
2014-06-28 18:13:01 -0400

    Still stuck on FK relation in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/new
   def new
+    @entry = Entry.find(params[:entry_id])
     @comment = Comment.new
   end
 
@@ -33,7 +34,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { render @entry, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
