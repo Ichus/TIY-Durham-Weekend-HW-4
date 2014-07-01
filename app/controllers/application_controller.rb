1bcf6327810e089d3498cca861f5d25eb4207000
Ichus <Ichus.local>
2014-07-01 10:54:23 -0400

    If the user is logged in they will be redirected to the create a post page if they navigate to the home page or login page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :logged_in_redirect
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in_redirect
+    if current_user
+      redirect_to users_path
+    end
   end
 end
